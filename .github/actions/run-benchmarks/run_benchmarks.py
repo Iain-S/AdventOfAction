8d0813172097732dffc08ace466b0ca1c5f13287
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-02-10 14:47:57 +0000

    Pyre-check

--- .github/actions/run-benchmarks/run_benchmarks.py
+++ .github/actions/run-benchmarks/run_benchmarks.py
@@ -3,15 +3,17 @@
 import os
 import subprocess
 import time
+from pathlib import Path
+from typing import Final, Optional, Mapping
 
 # Languages and their commands
-RUNTIMES = {".py": "python3", ".js": "node", ".sh": "bash"}
+RUNTIMES: Final = {".py": "python3", ".js": "node", ".sh": "bash"}
 
 
-def measure_execution_time(filepath):
+def measure_execution_time(filepath: Path) -> Optional[str]:
     ext = os.path.splitext(filepath)[1]
     if ext not in RUNTIMES:
-        return None  # Skip unsupported files
+        return "None"  # Skip unsupported files
 
     # Use the licence as input while testing.
     command = [RUNTIMES[ext], filepath, "./LICENSE"]
@@ -24,7 +26,7 @@ def measure_execution_time(filepath):
     return f"{time.time() - start:.3f} sec"
 
 
-def update_readme(the_results):
+def update_readme(the_results: Mapping[Path, str]) -> None:
     readme_path = "README.md"
     new_content = "\n## Benchmark Results\n"
     for the_path, time_taken in the_results.items():
@@ -35,12 +37,13 @@ def update_readme(the_results):
 
 
 if __name__ == "__main__":
-    results = {}
-    for root, _, files in os.walk("."):
-        if root.startswith("./day"):
-            for file in files:
-                if any(file.endswith(ext) for ext in RUNTIMES):
-                    path = os.path.join(root, file)
-                    results[path] = measure_execution_time(path)
+    results: dict[Path, str] = {}
+    path = Path(".")
+    for dirpath, _, filenames in path.walk():
+        if dirpath.parts and dirpath.parts[0].startswith("day_"):
+            for filename in filenames:
+                if any(filename.endswith(ext) for ext in RUNTIMES):
+                    if temp := measure_execution_time(dirpath / filename):
+                        results[dirpath / filename] = temp
 
     update_readme(results)
