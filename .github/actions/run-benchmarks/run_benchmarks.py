0842b3fded99a5cecf244267a730a0553fce1222
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-02-10 15:27:56 +0000

    Abstract base class

--- .github/actions/run-benchmarks/run_benchmarks.py
+++ .github/actions/run-benchmarks/run_benchmarks.py
@@ -1,26 +1,52 @@
 """Run every solution."""
 
-import os
 import subprocess
 import time
 from pathlib import Path
-from typing import Final, Optional, Mapping
+from typing import Final, Optional, Mapping, Type
+from abc import ABC, abstractmethod
+
+
+class Runtime(ABC):
+    @classmethod
+    @abstractmethod
+    def run(cls, *args, **kwargs) -> None:
+        """Run the code."""
+
+    @classmethod
+    @abstractmethod
+    def can_run(cls, *args, **kwargs) -> bool:
+        """Whether we can run the code"""
+
+
+class PythonRuntime(Runtime):
+    @classmethod
+    def run(cls, dirpath: Path, *args: str) -> None:
+        for filepath in dirpath.iterdir():
+            if filepath.suffix == ".py":
+                command = ["python", filepath]
+                print("Running", command)
+                subprocess.run(command)
+
+    @classmethod
+    def can_run(cls, dirpath: Path) -> bool:
+        for filepath in dirpath.iterdir():
+            if filepath.suffix == ".py":
+                return True
+        return False
+
 
 # Languages and their commands
-RUNTIMES: Final = {".py": "python3", ".js": "node", ".sh": "bash"}
+RUNTIMES: Final = {
+    PythonRuntime,
+}
 
 
-def measure_execution_time(filepath: Path) -> Optional[str]:
-    ext = os.path.splitext(filepath)[1]
-    if ext not in RUNTIMES:
-        return "None"  # Skip unsupported files
-
+def measure_execution_time(dirpath: Path, ext: Type[Runtime]) -> Optional[str]:
     # Use the licence as input while testing.
-    command = [RUNTIMES[ext], filepath, "./LICENSE"]
     start = time.time()
     try:
-        print("Running", command)
-        subprocess.run(command, capture_output=True, text=True, check=True)
+        ext.run(dirpath)
     except subprocess.CalledProcessError as e:
         return f"Error ({e.returncode})"
     return f"{time.time() - start:.3f} sec"
@@ -36,14 +62,18 @@ def update_readme(the_results: Mapping[Path, str]) -> None:
         f.write(new_content)
 
 
-if __name__ == "__main__":
+def main() -> None:
     results: dict[Path, str] = {}
     path = Path(".")
     for dirpath, _, filenames in path.walk():
         if dirpath.parts and dirpath.parts[0].startswith("day_"):
-            for filename in filenames:
-                if any(filename.endswith(ext) for ext in RUNTIMES):
-                    if temp := measure_execution_time(dirpath / filename):
-                        results[dirpath / filename] = temp
+            for ext in RUNTIMES:
+                if ext.can_run(dirpath):
+                    if temp := measure_execution_time(dirpath, ext):
+                        results[dirpath] = temp
 
     update_readme(results)
+
+
+if __name__ == "__main__":
+    main()
