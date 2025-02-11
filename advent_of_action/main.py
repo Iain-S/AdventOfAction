04352a95c9b3269697d5d336a8ffec155666a704
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-02-11 11:12:58 +0000

    ipython run

--- advent_of_action/main.py
+++ advent_of_action/main.py
@@ -78,7 +78,7 @@ def measure_execution_time(dirpath: Path, ext: RunnerFunc) -> str:
 
 def update_readme(the_results: Mapping[Path, str]) -> None:
     readme_path = "README.md"
-    new_content = "\n## Results\n"
+    new_content = "\n## Results\n\n"
     for the_path, time_taken in the_results.items():
         new_content += f"- `{the_path}`: {time_taken}\n"
 
