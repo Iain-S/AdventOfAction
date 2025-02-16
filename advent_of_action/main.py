2aa91d12e1f150abb0d4c9370506ca1ede39df9e
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-02-16 22:42:13 +0000

    Runner classes

--- advent_of_action/main.py
+++ advent_of_action/main.py
@@ -7,16 +7,16 @@ from subprocess import CalledProcessError, TimeoutExpired, run
 from typing import Final
 
 from advent_of_action import runners
-from advent_of_action.runners import RunnerFunc
+from advent_of_action.runners import Part, Runner
 
 # Languages and their commands
-RUNTIMES: Final[dict[str, RunnerFunc]] = {
-    "python": runners.python,
-    "racket": runners.racket,
-    "rust": runners.rust,
-    "fsharp": runners.fsharp,
-    "ocaml": runners.ocaml,
-    "jupyter": runners.jupyter,
+RUNTIMES: Final[dict[str, Runner]] = {
+    "python": runners.PythonRunner,
+    # "racket": runners.racket,
+    "rust": runners.RustRunner,
+    # "fsharp": runners.fsharp,
+    # "ocaml": runners.ocaml,
+    # "jupyter": runners.jupyter,
 }
 
 type Day = str
@@ -32,11 +32,12 @@ type Stats = tuple[Stat, Stat]
 
 def measure_execution_time(answers: tuple[str, str], dirpath: Path, ext: RunnerFunc) -> Stats:
     """Measure the execution time of a solution."""
+    ext.setup()
 
-    def inner(part: str, answer: str) -> Stat:
+    def inner(part: Part, answer: str) -> Stat:
         """Use the runner to measure the execution time of one part."""
         try:
-            kilobytes, seconds, output = ext(dirpath, part)
+            kilobytes, seconds, output = ext(dirpath)
             if output != answer:
                 print(f"Incorrect answer for part {part}: {output}")
                 return "", "", "Different answer"
@@ -50,7 +51,9 @@ def measure_execution_time(answers: tuple[str, str], dirpath: Path, ext: RunnerF
 
         return f"{seconds:.2f}", f"{kilobytes}", ""
 
-    return inner("one", answers[0]), inner("two", answers[1])
+    result = tuple(map(inner, zip((Part), answers, strict=False)))
+    ext.teardown()
+    return result
 
 
 def from_table(table: str) -> dict[Run, Stats]:
@@ -61,9 +64,9 @@ def from_table(table: str) -> dict[Run, Stats]:
         if not line:
             break
         day, lang, person, part, seconds, kb, notes = line[1:-1].split(" | ")
-        if part == "one":
+        if part == Part.ONE:
             part_one = (seconds.strip(), kb.strip(), notes.strip())
-        elif part == "two":
+        elif part == Part.TWO:
             assert part_one is not None
             results[(day.strip(), lang.strip(), person.strip())] = (
                 part_one,
@@ -82,7 +85,7 @@ def to_table(results: Mapping[Run, Stats]) -> str:
     table += "| --- | --- | --- | --- | --- | --- | --- |\n"
     for the_run, stats in results.items():
         day, language, person = the_run
-        for (seconds, kilobytes, notes), part in zip(stats, ("one", "two"), strict=False):
+        for (seconds, kilobytes, notes), part in zip(stats, (Part.ONE, Part.TWO), strict=False):
             table += f"| {day} | {language} | {person} | {part} | {seconds} | {kilobytes} | {notes} |\n"
     return table
 
--- advent_of_action/runners.py
+++ advent_of_action/runners.py
@@ -2,20 +2,47 @@
 
 import os
 import subprocess
+from abc import ABC
 from collections.abc import Callable
+from enum import StrEnum
 from pathlib import Path
 
-type Triple = tuple[int, float, str]
-type RunnerFunc = Callable[[Path, str], Triple]
+type kilobytes = int
+type seconds = float
+type output = str
+type Triple = tuple[kilobytes, seconds, output]
+type RunnerFunc = Callable[[Path], Triple]
 
 
-def execute_command(command: list[str | Path]) -> Triple:
+# make an enum for part one or two
+class Part(StrEnum):
+    ONE = "one"
+    TWO = "two"
+
+
+class Runner(ABC):
+    """A runner for a programming language."""
+
+    @classmethod
+    def setup(cls, dirpath: Path) -> Triple:
+        raise NotImplementedError
+
+    @classmethod
+    def run(cls, dirpath: Path, part: Part) -> Triple:
+        raise NotImplementedError
+
+    @classmethod
+    def teardown(cls, dirpath: Path) -> Triple:
+        raise NotImplementedError
+
+
+def execute_command(command: list[str | Path], timeout: float = float(os.environ["TIMEOUT_SECONDS"])) -> Triple:
     """Execute a command and return the memory usage, time and stdout."""
     print("Running", command)
     result = subprocess.run(
         ["/usr/bin/time", "-f", "%M,%S,%U"] + command,
         capture_output=True,
-        timeout=float(os.environ["TIMEOUT_SECONDS"]),
+        timeout=timeout,
         text=True,
         check=True,
     )
@@ -23,31 +50,53 @@ def execute_command(command: list[str | Path]) -> Triple:
     return int(kilobytes), float(sys_seconds) + float(user_seconds), result.stdout.strip()
 
 
-def python(dirpath: Path, part: str) -> Triple:
+class PythonRunner(Runner):
     """Run a Python solution."""
-    return execute_command(["python", str(dirpath / "solution.py"), part])
+
+    @classmethod
+    def setup(cls, dirpath: Path) -> Triple:
+        return execute_command(["pip", "install", "-r", str(dirpath / "requirements.txt")])
+
+    @classmethod
+    def run(cls, dirpath: Path, part: Part) -> Triple:
+        return execute_command(["python", str(dirpath / "solution.py"), part.value])
+
+    @classmethod
+    def teardown(cls, dirpath: Path) -> Triple:
+        return execute_command(["pip", "uninstall", "-r", str(dirpath / "requirements.txt")])
 
 
-def racket(dirpath: Path, part: str) -> Triple:
-    """Run a Racket solution."""
-    return execute_command(["racket", str(dirpath / "solution.rkt"), part])
+# def racket(dirpath: Path, part: str) -> Triple:
+#     """Run a Racket solution."""
+#     return execute_command(["racket", str(dirpath / "solution.rkt"), part])
 
 
-def rust(dirpath: Path, part: str) -> Triple:
+class RustRunner(Runner):
     """Run a Rust solution."""
-    return execute_command(["cargo", "run", "--quiet", "--manifest-path", str(dirpath / "Cargo.toml"), part])
+
+    @classmethod
+    def setup(cls, dirpath: Path) -> Triple:
+        return execute_command(["cargo", "build", "--manifest-path", str(dirpath / "Cargo.toml")])
+
+    @classmethod
+    def run(cls, dirpath: Path, part: Part) -> Triple:
+        return execute_command(["cargo", "run", "--quiet", "--manifest-path", str(dirpath / "Cargo.toml"), part.value])
+
+    @classmethod
+    def teardown(cls, dirpath: Path) -> Triple:
+        return 0, 0, "Nothing to do"
 
 
-def fsharp(dirpath: Path, part: str) -> Triple:
-    """Run an F# solution."""
-    return execute_command(["dotnet", "fsi", str(dirpath / "solution.fsx"), part])
+# def fsharp(dirpath: Path, part: str) -> Triple:
+#     """Run an F# solution."""
+#     return execute_command(["dotnet", "fsi", str(dirpath / "solution.fsx"), part])
 
 
-def ocaml(dirpath: Path, part: str) -> Triple:
-    """Run an OCaml solution."""
-    return execute_command(["ocaml", str(dirpath / "solution.ml"), part])
+# def ocaml(dirpath: Path, part: str) -> Triple:
+#     """Run an OCaml solution."""
+#     return execute_command(["ocaml", str(dirpath / "solution.ml"), part])
 
 
-def jupyter(dirpath: Path, part: str) -> Triple:
-    """Run a Jupyter notebook."""
-    return execute_command(["ipython", "-c", f"%run {dirpath / 'solution.ipynb'}", part])
+# def jupyter(dirpath: Path, part: str) -> Triple:
+#     """Run a Jupyter notebook."""
+#     return execute_command(["ipython", "-c", f"%run {dirpath / 'solution.ipynb'}", part])
